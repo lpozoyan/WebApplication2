adbdc748df23531c1743d6fc90975556eae7174d
stud <stud@107-5>
2023-04-11 11:41:44 +0400

    Добавьте файлы проекта.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet ("hello")]
+        public ActionResult<string> HelloApi (string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            { 
+                return StatusCode(4555, "lusine");
+             
+            }
+            return "hello " + name;
+        }
+
+        [HttpGet("authorization")]
+        public ActionResult<bool> Authorization(AuthorizationRequst requst)
+        {
+            if (requst.login == "admin" && requst.password == "123")
+                return false;
+            else return true;
+        }
+    }
+
+    public class AuthorizationRequst
+    {
+        public string login { get; set; }   
+        public string password { get; set; }
+    }
+}
